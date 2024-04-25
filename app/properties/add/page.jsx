6cb50a1102245df7ea27846dd6adc7eba8d6cb38
heliosalesjr <heliosalesjr@gmail.com>
2024-04-25 10:30:08 -0300

    3 random properties at the home page

--- app/properties/add/page.jsx
+++ app/properties/add/page.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const AddPage = () => {
   return (
-    <Link href="/" className='text-purple-900 underline hover:text-blue-500'>Go back home!</Link>
+    <Link href="/">Add page</Link>
   )
 }
 
